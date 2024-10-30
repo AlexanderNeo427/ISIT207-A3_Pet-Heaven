bd588f6c54f96f879cb1edf76004b50811b007b8
Alexander Neo <AlexanderNeo427@gmail.com>
2024-10-30 15:33:27 +0800

    Setup firebase hosting, seems to be working so far.....

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from "react"
+// import React from "react"
 
 function App() {
-   const [count, setCount] = React.useState<number>(0)
+   // const [count, setCount] = React.useState<number>(0)
 
    return (
       <div className='outline-1 outline outline-red-600 flex flex-col justify-center items-center w-full h-full'>
          <div className='flex justify-center items-center text-2xl'>
-            Le Test
+            Hosting Firebase test
          </div>
       </div>
    )
