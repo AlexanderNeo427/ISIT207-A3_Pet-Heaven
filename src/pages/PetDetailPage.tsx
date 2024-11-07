a6a4fe74dd9bde1e5dbc26d71e8af306af3c16ec
Alexander Neo <AlexanderNeo427@gmail.com>
2024-11-07 14:55:08 +0800

    WIP the "Pet Detail" page - doing the formatting and all that stuff

--- src/pages/PetDetailPage.tsx
+++ src/pages/PetDetailPage.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import FooterSection from '../components/sections/FooterSection'
-import { PET_API_TYPE, PetApiData } from '../others/Globals'
+import { CatBreedData, DogBreedData, PET_API_TYPE, PetApiData } from '../others/Globals'
 import { useSearchParams } from 'react-router-dom'
 import axios from 'axios'
 
@@ -14,9 +14,9 @@ const PetDetailPage: React.FC = () => {
       const pet_id = m_searchParams.get('pet_id')
       const breed_id = m_searchParams.get('breed_id')
 
-      console.log("PetDetailPage - Pet Type: ", pet_type)
-      console.log("PetDetailPage - Pet ID: ", pet_id)
-      console.log("PetDetailPage - Breed ID: ", breed_id)
+      // console.log("PetDetailPage - Pet Type: ", pet_type)
+      // console.log("PetDetailPage - Pet ID: ", pet_id)
+      // console.log("PetDetailPage - Breed ID: ", breed_id)
 
       const imgEndpointURL = pet_type === "dog" ? 
          `https://api.thedogapi.com/v1/images/${pet_id}` :
@@ -65,12 +65,39 @@ const PetDetailPage: React.FC = () => {
 
    return (
       <main>
-         <Navbar />{
+         <Navbar useSticky/>{
             m_petApiData && 
-            <section className='min-h-[30rem] text-center'>
-               <img src={m_petApiData.imgURL} alt="" />
-               <div>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi fuga officiis eum ipsam eos voluptatum, cupiditate aperiam atque sequi perspiciatis corporis, quae, vero eius ipsa cumque provident pariatur deleniti facilis!
+            <section className='min-h-[30rem] text-center flex flex-col justify-start items-center'>
+
+               {/* ---- BANNER IMAGE ---- */}
+               <div style={{
+                  backgroundImage: `url(${m_petApiData.imgURL})`
+               }} className='w-full h-96 mt-margin-xxs mb-margin-xl'></div>
+
+               {/* --- --- */}
+               <div className='max-w-4xl flex flex-col md:flex-row mb-margin-xl mx-margin-s'>
+
+                  <div className='text-center flex-col mb-margin-xl mr-margin-xl'>
+                     <div className='flex items-start justify-between mb-margin-s'>
+                        <h1 className='font-bold text-3xl'>{m_petApiData.breedData?.breed.toUpperCase()}</h1>
+                        <button className='font-semibold rounded-lg outline outline-2 outline-primary-400 px-5 py-3'>Favourite</button>
+                     </div>
+                     <p className='text-left'>{
+                        m_petApiData.apiType === PET_API_TYPE.DOG ? 
+                        `${m_petApiData.breedData?.breed}'s are bred for ${(m_petApiData.breedData as DogBreedData).bredFor}`: 
+                        `${(m_petApiData.breedData as CatBreedData).description}`
+                     }</p>
+                  </div>
+                  <div className='
+                     flex flex-col justify-center items-center rounded-xl
+                     w-full max-w-72 text-center text-text-50 bg-white
+                  '>
+                     <span className='text-lg font-medium mb-margin-xxs'>Adoption Fee</span>
+                     <span className='text-xl font-semibold mb-margin-l'>$150.0</span>
+                     <button className='
+                        text-text-950 bg-primary-500 hover:bg-primary-600 px-6 py-2 rounded-lg
+                     '>Adopt Me</button>
+                  </div>
                </div>
             </section>
          }<FooterSection />
