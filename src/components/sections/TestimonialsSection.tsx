69d72e96bd02b3a01306f91c775ef4f6e7489f47
Alexander Neo <AlexanderNeo427@gmail.com>
2024-11-09 16:01:36 +0800

    Review + review content for each testimonial card. Additional styling

--- src/components/sections/TestimonialsSection.tsx
+++ src/components/sections/TestimonialsSection.tsx
@@ -12,7 +12,32 @@ import dog_4 from '../../assets/images/testimonials/dog_04.jpg'
 import dog_5 from '../../assets/images/testimonials/dog_05.jpg'
 import dog_6 from '../../assets/images/testimonials/dog_07.jpg'
 import dog_7 from '../../assets/images/testimonials/dog_08.jpg'
-import { keyframes } from 'framer-motion'
+
+const testimonialDataTop: TestimonialData[] = [
+   { imgURL: cat_1 },
+   { imgURL: dog_1, reviewer: "Abel Choy", content: "Saepe repellendus reiciendis ducimus at rerum veritatis! Voluptatum at praesentium laborum est sint illum deserunt fugiat, aliquam nobis unde amet, facilis laborum expedita beatae blanditiis autem, ab vitae soluta!" },
+   { imgURL: dog_2 },
+   { imgURL: dog_3 },
+   { imgURL: cat_2 },
+   { imgURL: rabbit_1, reviewer: "Jionggo Sapit", content: "Hic illo cum nobis earum harum, facilis laborum expedita beatae blanditiis autem, ab vitae soluta! Fugiat delectus cum recusandae itaque excepturi ipsum!" },
+]
+
+const testimonialDataBottom: TestimonialData[] = [
+   { imgURL: dog_4, reviewer: "Bob Dylan", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, harum facilis quam a incidunt non veritatis!" },
+   { imgURL: cat_3 },
+   { imgURL: dog_5 },
+   { imgURL: dog_6, reviewer: "Bob Dylan", content: "Quod molestias saepe repellendus reiciendis ducimus at rerum veritatis! Voluptatum at praesentium laborum est. Fugiat unde amet, hic illo cum nobis earum harum, facilis" },
+   { imgURL: cat_4 },
+   { imgURL: rabbit_2 },
+   { imgURL: dog_7 },
+]
+
+interface CarouselItemProps {
+   widthRem: number
+   heightRem: number
+   offset: number
+   testimonialData: TestimonialData
+}
 
 interface TestimonialData {
    imgURL: string
@@ -25,58 +50,60 @@ interface TestimonialCarouselProps {
    testimonialData: TestimonialData[]
 }
 
-const testimonialDataTop: TestimonialData[] = [
-   { imgURL: cat_1 },
-   { imgURL: dog_1 },
-   { imgURL: dog_2 },
-   // { imgURL: dog_3 },
-   // { imgURL: cat_2 },
-   // { imgURL: rabbit_1 },
-]
-
-const testimonialDataBottom: TestimonialData[] = [
-   { imgURL: dog_4 },
-   // { imgURL: cat_3 },
-   // { imgURL: dog_5 },
-   // { imgURL: dog_6 },
-   // { imgURL: cat_4 },
-   // { imgURL: rabbit_2 },
-   // { imgURL: dog_7 },
-]
-
-interface CarouselItemProps {
-   widthRem: number
-   heightRem: number
-   offset: number
-   testimonialData: TestimonialData
-}
-
 const CarouselItem: React.FC<CarouselItemProps> = props => {
    return (
       <div
-         style={{ 
-            width: props.widthRem + "rem", 
-            height: props.heightRem + "rem", 
+         style={{
+            width: props.widthRem + "rem",
+            height: props.heightRem + "rem",
             transform: `translateX(${props.offset}rem)`
          }}
-         className='absolute rounded-xl outline outline-2 outline-red-400'
+         className='absolute px-5 py5'
       >
-         <div
-            style={{ backgroundImage: `url(${props.testimonialData.imgURL})` }}
-            className='w-full h-full bg-cover rounded-xl'
-         ></div>
+         <div className='relative w-full h-full'>
+            <div
+               style={{ backgroundImage: `url(${props.testimonialData.imgURL})` }}
+               className='w-full h-full absolute bg-cover rounded-xl'
+            ></div>
+            {(props.testimonialData.reviewer && props.testimonialData.content) &&
+               <div
+                  style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
+                  className='absolute top-0 left-0 right-0 bottom-0 rounded-xl flex flex-col justify-end items-start'>
+                  <div className='mx-margin-l mb-margin-l'>
+                     <p className='text-text-950 text-lg text-left font-bold'>
+                        <i>
+                           <span style={{ color: 'rgb(20, 190, 30)' }} className='text-2xl'>"</span>
+                           {props.testimonialData.content}
+                           <span style={{ color: 'rgb(20, 190, 30)' }} className='text-2xl'>"</span>
+                        </i>
+                     </p>
+                     <span style={{ color: 'rgb(20, 190, 30)' }} className='text-secondary-800 font-bold text-2xl'>
+                        - {props.testimonialData.reviewer}
+                     </span>
+                  </div>
+               </div>}
+         </div>
       </div>
    )
 }
 
 const TestimonialCarousel: React.FC<TestimonialCarouselProps> = props => {
-   const WIDTH_REM = 18
-   const HEIGHT_REM = 13
+   const WIDTH_REM = 27
+   const HEIGHT_REM = 17
 
+   const [m_windowWidth, setWindowWidth] = useState<number>(-1)
    const [m_carouselOffsets, setCarouselOffets] = useState<number[]>([])
    const [m_parentWidthRem, setParentWidthRem] = useState<number>(0)
    const m_animHandle = useRef<number>(-1)
    const m_divRef = useRef<HTMLDivElement>(null)
+
+   useEffect(() => {
+      const windowResizeHandler = () => {
+         setWindowWidth(window.innerWidth)
+      }
+      window.addEventListener('resize', windowResizeHandler)
+      return () => window.removeEventListener('resize', windowResizeHandler)
+   }, [])
 
    // Listen for changes to the "testimonial data" passed via props
    // The nth carousel item can find out it's corresponding translateX via m_carouselOffset[n]
@@ -84,9 +111,7 @@ const TestimonialCarousel: React.FC<TestimonialCarouselProps> = props => {
    // Value of m_carouseOffset will be updated via the requestAnimationFrame in a useEffect below
    useEffect(() => {
       setCarouselOffets(
-         props.testimonialData.map((_, idx) => {
-            return WIDTH_REM * idx
-         })
+         props.testimonialData.map((_, idx) => WIDTH_REM * idx)
       )
       if (m_animHandle.current) {
          cancelAnimationFrame(m_animHandle.current)
@@ -103,26 +128,29 @@ const TestimonialCarousel: React.FC<TestimonialCarouselProps> = props => {
    }, [m_divRef.current])
 
    useEffect(() => {
-      const SCROLL_SPEED = 5
+      const SCROLL_SPEED = 2
 
       let prevTime = 0
       const onTick = (timeElapsed: number): void => {
          const deltaTime = (timeElapsed - prevTime) / 1000;
          prevTime = timeElapsed;
 
+         const WRAP_SIZE = WIDTH_REM * (props.testimonialData.length - 1)
+
+         // setCarouselOffets(props.testimonialData.map((_, idx) => WIDTH_REM * idx));
          setCarouselOffets(prevOffsets => {
             return prevOffsets.map(offset => {
                const newOffset = offset +
-                  (props.reverseScrollDir ?
-                     SCROLL_SPEED * deltaTime : -SCROLL_SPEED * deltaTime)
+                  (props.reverseScrollDir ? SCROLL_SPEED * deltaTime : -SCROLL_SPEED * deltaTime)
+
                if (props.reverseScrollDir) {
                   if (newOffset > m_parentWidthRem) {
-                     return newOffset - m_parentWidthRem - WIDTH_REM
+                     return newOffset - WRAP_SIZE - WIDTH_REM
                   }
                }
                else {
                   if (newOffset < -WIDTH_REM) {
-                     return newOffset + m_parentWidthRem + WIDTH_REM
+                     return newOffset + WRAP_SIZE + WIDTH_REM
                   }
                }
                return newOffset
@@ -131,17 +159,17 @@ const TestimonialCarousel: React.FC<TestimonialCarouselProps> = props => {
          m_animHandle.current = window.requestAnimationFrame(onTick)
       }
       m_animHandle.current = window.requestAnimationFrame(onTick)
-      return () => { 
+      return () => {
          if (m_animHandle.current) {
-            cancelAnimationFrame(m_animHandle.current) 
+            cancelAnimationFrame(m_animHandle.current)
          }
       }
-   }, [props.testimonialData.length, m_parentWidthRem])
+   }, [props.testimonialData.length, m_parentWidthRem, m_windowWidth])
 
    return (
       <div
-         style={{ width: "96%", height: HEIGHT_REM + "rem" }}
-         ref={m_divRef} className='relative overflow-hidden outline outline-2 outline-red-700'>
+         style={{ width: "100%", height: HEIGHT_REM + "rem", minWidth: m_parentWidthRem + "rem" }}
+         ref={m_divRef} className='relative overflow-hidden'>
          {props.testimonialData.map((data, idx) => {
             return (
                <CarouselItem
@@ -157,7 +185,7 @@ const TestimonialCarousel: React.FC<TestimonialCarouselProps> = props => {
 
 const TestimonialsSection: React.FC = () => {
    return (
-      <section className='min-h-96 bg-gray-600 py-margin-xl flex flex-col items-center gap-6'>
+      <section className='min-h-96 bg-background-600 py-margin-xl flex flex-col items-center gap-6'>
          <TestimonialCarousel testimonialData={testimonialDataTop} />
          <TestimonialCarousel reverseScrollDir testimonialData={testimonialDataBottom} />
       </section>
